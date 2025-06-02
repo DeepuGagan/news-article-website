10cc85c95287a899ad8788b1c63b5a31b5213942
DeepuGagan <gagandeepu717@gmail.com>
2025-06-02 15:21:31 +0530

    Add Header component with navigation menu

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "./Header.css";
+import logo from "../../Assets/logo.png";
+import { IoMenu, IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenuItemClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    if (window.innerWidth <= 1136) {
+      setShowMenu(false);
+    }
+  };
+
+  return (
+    <section id="header">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+      <div className={showMenu ? "mobile-menu-items" : "menu-items"}>
+        <ul>
+          <li>
+            <Link to="/" onClick={handleMenuItemClick}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/breakingNews" onClick={handleMenuItemClick}>
+              Breaking News
+            </Link>
+          </li>
+          <li>
+            <Link to="/sports" onClick={handleMenuItemClick}>
+              Sports
+            </Link>
+          </li>
+          <li>
+            <Link to="/entertainment" onClick={handleMenuItemClick}>
+              Entertainment
+            </Link>
+          </li>
+          <li>
+            <Link to="/technology" onClick={handleMenuItemClick}>
+              Technology
+            </Link>
+          </li>
+          <li>
+            <Link to="/world" onClick={handleMenuItemClick}>
+              World
+            </Link>
+          </li>
+          <li>
+            <Link to="/business" onClick={handleMenuItemClick}>
+              Business
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="headerButton">
+
+        <button
+          className="mobile-menu-icons"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          {showMenu ? <IoClose size="30px" /> : <IoMenu size="30px" />}
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
